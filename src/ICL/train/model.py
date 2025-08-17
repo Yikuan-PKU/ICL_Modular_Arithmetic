1a493bdb9898b0752cedfd507361e4db8599cf9d
Jing Liu <84009338+Jing-L97@users.noreply.github.com>
2025-08-17 17:08:17 +0200

    add exp test codes

--- src/ICL/train/model.py
+++ src/ICL/train/model.py
@@ -23,6 +23,8 @@ from transformers import (
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# TODO: add special tokens for seq boundary markers
+
 
 @dataclass
 class RHMTrainingConfig:
